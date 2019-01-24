7e89aba2a3656f0b1a8fa2009853771f9415fcd5
Levi Blodgett <levi_blodgett@outlook.com>
2019-01-23 20:01:26 -0600

    Changing view profile spacing

--- ui.js
+++ ui.js
@@ -10,7 +10,7 @@ class UI {
       <div class="row">
         <div class="col-md-3">
           <img class="img-fluid mb-2" src="${user.avatar_url}">
-          <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+          <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-3 mt-3">View Profile</a>
         </div>
         <div class="col-md-9">
           <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
